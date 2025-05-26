361626542e74653b4ea9fe4d258d7cbb26567b3a
ashish3377 <ashish.sheetal4@gmail.com>
2025-05-26 11:02:26 +0530

    Add files via upload

---
+++ src/components/ViewIngredientsButton/styles.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: 50,
+    width: 270,
+    marginTop: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    borderRadius: 100,
+    borderColor: '#f44336',
+    borderWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+    // backgroundColor: '#f44336'
+  },
+  text: {
+    fontSize: 14,
+    color: '#f44336'
+  }
+});
+
+export default styles;
---
+++ src/screens/Splash/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f44336",
+  },
+  photo: {
+    flex: 1,
+  },
+});
+
+export default styles;
